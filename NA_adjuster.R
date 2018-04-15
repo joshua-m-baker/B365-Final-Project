5c095065a844e371c00ce4f64a6545280072da98
Joshua Baker <joshuabaker97@gmail.com>
2018-04-15 16:50:28 -0400

    attempted to replace NAs

--- NA_adjuster.R
+++ NA_adjuster.R
@@ -1,13 +1,15 @@
-data.train <- read.csv("train.csv", header = TRUE,na.strings = "-1")
-data.test <- read.csv("test.csv", header = TRUE,na.strings = "-1")
+install.packages('mice')
+library('mice')
+setwd('C:\\Users\\Joshua\\Documents\\GitHub\\B365-Final-Project')
 
+data.train <- read.csv("train.csv", header = TRUE,na.strings = "-1" )
+data.test <- read.csv("test.csv", header = TRUE,na.strings = "-1")
+data.train[data.train<0] = NA
+data.test[data.test<0] = NA
 
 data.test = cbind(data.test,NA_count = rowSums(is.na(data.test))) 
 data.train = cbind(data.train,NA_count = rowSums(is.na(data.train))) 
 
 
-b = c(ps_car_03_cat,ps_car_05_cat)
-data.test <- subset(data.test,select = -c(ps_car_03_cat,ps_car_05_cat))
-data.train <- subset(data.train,select = -c(ps_car_03_cat,ps_car_05_cat))
-
-
+data.test <- subset(data.test,select = -c(ps_car_03_cat,ps_car_05_cat,ps_reg_03))
+data.train <- subset(data.train,select = -c(ps_car_03_cat,ps_car_05_cat,ps_reg_03))
--- code_josh.R
+++ code_josh.R
@@ -1,7 +1,11 @@
 library(corrplot)
-data.train = read.csv('C:\\Users\\Joshua\\Documents\\Data Mining Final Project\\train.csv')
+library(data.table)
+setwd('C:\\Users\\Joshua\\Documents\\GitHub\\B365-Final-Project')
+data.train = fread('train.csv')
+data.test = fread('test.csv')
 summary(data.train)
 head(data.train)
+summary(data.test)
 
 data.train[data.train == -1] <- NA
 data.train[data.train == -1.0] <- NA
@@ -9,3 +13,14 @@ data.train[data.train == -1.0] <- NA
 data.reduced = na.omit(data.train)
 x = cor(data.reduced)
 corrplot(x, method = "circle", type = 'upper')
+
+library('caret')
+tr = data.train
+tr$target = as.factor(tr$target)
+print(Sys.time())
+#load('model1.rda')
+model = train(tr[,3:59],tr$target,'nb',preProcess = 'knnImpute',
+              trControl = trainControl(method="cv",number=10))
+save(model, file = 'model1.rda')
+print(Sys.time())
+              
