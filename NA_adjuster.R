db3c323199d78672cb09bcd04c1daf210f94f920
whuibreg <whuibreg@indiana.edu>
2018-04-28 16:02:15 -0400

    hopefully fixed the issue

--- NA_adjuster.R
+++ NA_adjuster.R
@@ -1,6 +1,6 @@
-install.packages('mice')
-library('mice')
-setwd('C:\\Users\\Joshua\\Documents\\GitHub\\B365-Final-Project')
+#install.packages('mice')
+#library('mice')
+#setwd('C:\\Users\\Joshua\\Documents\\GitHub\\B365-Final-Project')
 
 data.train <- read.csv("train.csv", header = TRUE,na.strings = "-1" )
 data.test <- read.csv("test.csv", header = TRUE,na.strings = "-1")
@@ -11,5 +11,21 @@ data.test = cbind(data.test,NA_count = rowSums(is.na(data.test)))
 data.train = cbind(data.train,NA_count = rowSums(is.na(data.train))) 
 
 
-data.test <- subset(data.test,select = -c(ps_car_03_cat,ps_car_05_cat,ps_reg_03))
-data.train <- subset(data.train,select = -c(ps_car_03_cat,ps_car_05_cat,ps_reg_03))
+data.test <- subset(data.test,select = -c(id,ps_car_03_cat,ps_car_05_cat,ps_reg_03))
+data.train <- subset(data.train,select = -c(id,ps_car_03_cat,ps_car_05_cat,ps_reg_03))
+
+training_classes = data.train$target
+data.train <- subset(data.train,select = -c(target))
+training_size = length(data.train)
+
+total_data = rbind(data.train, data.test)
+
+col_means = colMeans(total_data,na.rm = TRUE)
+for(i in 1:ncol(total_data)){
+  total_data[is.na(total_data[,i]), i] <- col_means[i]
+}
+
+summary(total_data)
+# PCA Stuff
+pca <- prcomp(total_data)
+summary(pca)
