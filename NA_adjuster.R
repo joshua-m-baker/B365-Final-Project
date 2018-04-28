0a69950028067f5568e5375bd6e616baa7fb3728
whuibreg <whuibreg@indiana.edu>
2018-04-28 17:55:08 -0400

    code to make submission files

--- NA_adjuster.R
+++ NA_adjuster.R
@@ -1,6 +1,7 @@
 #install.packages('mice')
 #library('mice')
 #setwd('C:\\Users\\Joshua\\Documents\\GitHub\\B365-Final-Project')
+options(scipen = 50)
 
 data.train <- read.csv("train.csv", header = TRUE,na.strings = "-1" )
 data.test <- read.csv("test.csv", header = TRUE,na.strings = "-1")
@@ -10,6 +11,7 @@ data.test[data.test<0] = NA
 data.test = cbind(data.test,NA_count = rowSums(is.na(data.test))) 
 data.train = cbind(data.train,NA_count = rowSums(is.na(data.train))) 
 
+test_ids = data.test$id
 
 data.test <- subset(data.test,select = -c(id,ps_car_03_cat,ps_car_05_cat,ps_reg_03))
 data.train <- subset(data.train,select = -c(id,ps_car_03_cat,ps_car_05_cat,ps_reg_03))
@@ -41,5 +43,7 @@ pred = predict(model, test,"raw")
 pred
 pred[,2]
 
+submission = cbind(id = test_ids, target = pred[,2])
+write.table(submission, file="submission.csv", row.names = FALSE, col.names = TRUE, sep=",")
 #model = train(training,training_classes,'nb',trControl=trainControl(method='cv',number=10))
 
